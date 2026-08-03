a16e08289088171d071c43c4139a2024f4009279
Ctrl-De4ault <thesasukeuchhia@gmail.com>
2026-08-04 00:55:18 +0530

    BS-2

---
+++ ceil.cpp
@@ -0,0 +1,16 @@
+int findCeil(vector<int>& arr, int x) {
+    // code here
+    int n=arr.size();
+    int ans=-1;
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>=x){
+            ans=mid;
+            high=mid-1;
+        }
+        else low=mid+1;
+    }
+    return ans;
+}
